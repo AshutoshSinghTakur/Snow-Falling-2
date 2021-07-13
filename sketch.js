efdd7275ce73b81396295af27b8c5e0ade15c349
AshutoshSinghTakur <ashuthakur1718@gmail.com>
2021-07-13 19:17:58 +0530

    boy_animation

--- sketch.js
+++ sketch.js
@@ -6,19 +6,22 @@ function preload(){
   back3 = loadImage("snow3.jpg");
   snowfall1 = loadImage("snow4.png");
   snowfall2 = loadImage("snow5.png");
+  boyImg  = loadAnimation("boy1.png","boy2.png");
+  boyImg1 = loadAnimation("boy3.png","boy4.png")
+  boyImg2 = loadAnimation("boy5.png","boy6.png");
+  boyImg3 = loadAnimation("boy7.png","boy8.png");
 
   //getBackgroundImg();
 }
 
 function setup() {
   createCanvas(1000,600);
-
   
 }
 
 function draw() {
   //if(backgroundImg){
-  background(back3);  
+  background(back2);  
   //}
 
   if(frameCount % 30 == 0){
@@ -31,6 +34,22 @@ function draw() {
     snow2.addImage(snowfall2);
     snow2.scale = 0.1
     snow2.velocityY = 2
+  }
+
+  if(frameCount % 170 == 0){
+    position = Math.round(random(1,2));
+    var boy = createSprite(1000,450)
+    if(position == 1){
+     boy.x = 1000; 
+     boy.velocityX = -5
+     boy.addAnimation("moving",boyImg)
+     }else{
+      boy.x = 0;
+      boy.velocityX = 5
+      boy.addAnimation("moving1", boyImg1)
+     }
+    boy.scale = 0.7 
+
   }
 
   drawSprites();
